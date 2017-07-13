6acac6a82ed9f988ae8da18d6fd75f84228faf3d
Limby72 <Limb720@gmail.com>
2017-07-13 15:46:45 -0400

    commit

--- RideWithMeGood/AppDelegate.swift
+++ RideWithMeGood/AppDelegate.swift
@@ -43,25 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         -> Bool {
             
             return GIDSignIn.sharedInstance().handle(url, sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String, annotation: [:])
+            
+//            let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+//            
+//            return handled
     }
     
-    
-    
-    
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        
-        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
-        
-        return handled
-    }
-    
-
-    
-    
-    
-    
-
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
