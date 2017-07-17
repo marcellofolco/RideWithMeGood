53ee1e136deb7423da269366846372f349f7e728
Marcello Folco <march_846@hotmail.com>
2017-07-17 16:26:59 -0400

    Weather VC and Messages VC added

---
+++ RideWithMeGood/WeatherVC.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherVC.swift
+//  RideWithMeGood
+//
+//  Created by Marcello Folco on 2017-07-17.
+//  Copyright © 2017 Marcello Folco. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
