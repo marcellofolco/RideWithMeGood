e904a4a49d81b5b0b955fa4583470a6d5ee2337d
Marcello Folco <march_846@hotmail.com>
2017-07-19 17:22:22 -0400

    Mapbox framework added

--- RideWithMeGood/MapViewController.swift
+++ RideWithMeGood/MapViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 import MapKit
+import Mapbox
 import CoreLocation
 
-class MapViewController: UIViewController, UIBarPositioningDelegate, MKMapViewDelegate, CLLocationManagerDelegate{
+class MapViewController: UIViewController, UIBarPositioningDelegate, MKMapViewDelegate, CLLocationManagerDelegate, MGLMapViewDelegate{
     
     @IBOutlet weak var navBar: UINavigationBar!
-    @IBOutlet weak var map: MKMapView!
+   // @IBOutlet weak var map: MKMapView!
+    
     @IBOutlet weak var infoRef: UIView!
+    @IBOutlet weak var mapView: MGLMapView!
     
     var manager = CLLocationManager()
     
@@ -28,8 +31,21 @@ class MapViewController: UIViewController, UIBarPositioningDelegate, MKMapViewDe
         manager.distanceFilter = 50
         manager.startUpdatingLocation()
         
+        mapView.delegate = self
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: 45.4882780469, longitude: -73.5837409984)
+        point.title = "Lasalle College"
+        point.subtitle = "2000 Saint-Catherine St W, Montreal, QC H3H 2T3"
+        
+        mapView.addAnnotation(point)
+        
         infoRef.isHidden = true
         
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        
+        return true
     }
     
     func position(for bar: UIBarPositioning) -> UIBarPosition{
@@ -54,13 +70,13 @@ class MapViewController: UIViewController, UIBarPositioningDelegate, MKMapViewDe
         
         let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
         
-        map.setRegion(region, animated: false)
+        //map.setRegion(region, animated: false)
         
         let pin = MKPointAnnotation()
         pin.coordinate.latitude = userLocation.coordinate.latitude
         pin.coordinate.longitude = userLocation.coordinate.longitude
         pin.title = "Your Movement Line"
-        map.addAnnotation(pin)
+        //map.addAnnotation(pin)
         
     }
     
